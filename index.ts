c6f766eac3a4543e6628598878805516c4e10b2f
Elhanan Flesch <elhanan@utopia.tech>
2024-09-26 16:59:02 +0300

    initial implementation

--- index.ts
+++ index.ts
@@ -1,4 +1,41 @@
-console.log('Hello via Bun!');
-const i = () => {
-  return 2 + 2;
-};
+import fs from 'fs';
+import { Command } from 'commander';
+import { name, version, description } from './package.json';
+
+const program = new Command();
+
+program
+  .name(name)
+  .description(description)
+  .version(version)
+  .argument('inputPath')
+  .argument('outputPath')
+  .action(
+    (inputPath, outputPath) =>
+      new Promise((resolve, reject) => {
+        if (!fs.existsSync(inputPath)) {
+          reject('Cannot read from provided input file');
+          return;
+        }
+        let inputLines: string[];
+        try {
+          inputLines = fs.readFileSync(inputPath, { encoding: 'utf8' }).split('\n');
+        } catch {
+          reject('failed to parse input file');
+          return;
+        }
+        const outputLines = inputLines.map((line) =>
+          line.includes('OWNER TO') || line.includes('GRANT ALL') ? `-- ${line}` : line
+        );
+        const output = outputLines.join('\n');
+        try {
+          fs.writeFileSync(outputPath, output);
+        } catch {
+          reject('failed to write to output path');
+          return;
+        }
+        resolve();
+      })
+  );
+
+program.parse();
